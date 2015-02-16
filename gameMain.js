8509d0554d38c60e275f3297197bc91b0682598c
Michael Miriti <michael@miriti.ru>
2015-02-16 13:36:03 +0300

    sprite test

--- gameMain.js
+++ gameMain.js
@@ -1,60 +1,74 @@
-Ball = function () {
-    GameObject.call(this);
+Ball = function (t) {
+    PIXI.Sprite.call(this, t);
 
-    var g = new PIXI.Graphics();
+    this.rad = 20 + Math.random() * 100;
 
-    this.rad = 30 + Math.random() * 60;
+    this.width = this.rad;
+    this.height = this.rad;
+
+    this.pivot = new PIXI.Point(this.width / 2, this.height / 2);
 
     this.velicity = 30 + Math.random() * 200;
     this.vector = new Vector2(Math.random() * 100, Math.random() * 100);
     this.vector.lim(1);
-
-    g.beginFill(Math.random() * 0xffffff);
-    g.drawEllipse(0, 0, this.rad, this.rad);
-    g.endFill();
-
-    this.addChild(g);
 };
 
-Ball.prototype = Object.create(GameObject.prototype);
+Ball.prototype = Object.create(PIXI.Sprite.prototype);
 Ball.prototype.constructor = Ball;
-
-Ball.prototype.update = function (delta) {
-    this.x += this.vector.x * (this.velicity * delta);
-    this.y += this.vector.y * (this.velicity * delta);
-
-    if (this.x > window.innerWidth - this.rad) {
-        this.x = window.innerWidth - this.rad;
-        this.vector.x *= -1;
-    }
-
-    if (this.x < this.rad) {
-        this.x = this.rad;
-        this.vector.x *= -1;
-    }
-
-    if (this.y > window.innerHeight - this.rad) {
-        this.y = window.innerHeight - this.rad;
-        this.vector.y *= -1;
-    }
-
-    if (this.y < this.rad) {
-        this.y = this.rad;
-        this.vector.y *= -1;
-    }
-};
 
 GameMain = function () {
     GameState.call(this);
 
-    for (var i = 0; i < 5000; i++) {
-        var b = new Ball();
+    var texture = PIXI.Texture.fromImage("sprite.png");
+
+    var batch = new PIXI.SpriteBatch();
+
+    this.balls = [];
+
+    for (var i = 0; i < 10000; i++) {
+        var b = new Ball(texture);
         b.x = Math.random() * window.innerWidth;
         b.y = Math.random() * window.innerHeight;
 
-        this.addChild(b);
+        batch.addChild(b);
+
+        this.balls.push(b);
     }
+
+    this.addChild(batch);
+
 };
 
 GameMain.prototype = Object.create(GameState.prototype);
 GameMain.prototype.constructor = GameMain;
+
+GameMain.prototype.update = function (delta) {
+    for (var i = 0; i < this.balls.length; i++) {
+        (function (b) {
+            b.x += b.vector.x * (b.velicity * delta);
+            b.y += b.vector.y * (b.velicity * delta);
+
+            if (b.x > window.innerWidth - b.rad * 2) {
+                b.x = window.innerWidth - b.rad * 2;
+                b.vector.x *= -1;
+            }
+
+            if (b.x < 0) {
+                b.x = 0;
+                b.vector.x *= -1;
+            }
+
+            if (b.y > window.innerHeight - b.rad * 2) {
+                b.y = window.innerHeight - b.rad * 2;
+                b.vector.y *= -1;
+            }
+
+            if (b.y < 0) {
+                b.y = 0;
+                b.vector.y *= -1;
+            }
+
+            b.rotation += 1 * delta;
+        })(this.balls[i]);
+    }
+}
